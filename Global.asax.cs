d4520da2d4f34a6c8e15549d1785f0da77b2cdc5
Matthew Blott <Matt@Matthews-MBP.home>
2016-09-12 21:12:31 +0100

    initial commit

---
+++ Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using static NLog.LogManager;
+
+namespace aspnetnlog
+{
+  public class Global : HttpApplication
+  {
+    protected void Application_Start()
+    {
+      var logger = GetCurrentClassLogger();
+
+      logger.Info("Initialisation started - logged via Global");
+
+      Helper.Log("Initialisation started - logged via Helper");
+
+      AreaRegistration.RegisterAllAreas();
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+      logger.Info("Initialisation completed - logged via Global");
+
+      Helper.Log("Initialisation completed - logged via Helper");
+
+    }
+
+  }
+
+}
---
+++ Objects/Helper.cs
@@ -0,0 +1,33 @@
+﻿using static NLog.LogManager;
+
+public static class Helper
+{
+  public static void Log(string message)
+  {
+    var logger = GetCurrentClassLogger();
+
+    logger.Info(message);
+
+
+    //var environmentConnectionString = Environment.GetEnvironmentVariable("MyDbConnection", EnvironmentVariableTarget.User);
+
+    //connectionString = !string.IsNullOrEmpty(environmentConnectionString)
+    //                        ? environmentConnectionString
+    //                        : ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+
+    //var logger = NLog.LogManager.GetCurrentClassLogger();
+
+    //var anotherLogger = NLog.LogManager.GetLogger("name", typeof(Helper));
+
+    //NLog.LogLevel level;
+
+    //level = NLog.LogLevel.Debug;
+
+    //var message = "we have an error";
+
+    //logger.Log(level, message);
+
+
+  }
+
+}
